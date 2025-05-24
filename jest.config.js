53e295b347e515142bb12696e906e5bbecc0dd9f
Andreas Martin Aanerud <a.m.aanerud@gmail.com>
2025-05-24 20:33:39 +0200

    cleanup, priming for release

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-    testEnvironment: 'node',
-    transform: {
-        '^.+\\.(js|cjs)$': 'babel-jest'
-    },
-    moduleFileExtensions: ['js', 'cjs', 'json', 'node'],
-    testMatch: ['**/__tests__/**/*.(js|cjs)', '**/*.(test|spec).(js|cjs)']
-};
